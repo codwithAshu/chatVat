a9d098e4914551f3bd90f4ec7c14fc317ed50a52
Arshdeep Singh <arshdeep.intern@prepladder.com>
2025-05-13 18:31:14 +0530

    change axiospost

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,7 +40,7 @@ const LogIn = () => {
     }),
         onSubmit :async(values) => {
           try{
-    const response=await axios.post('http://localhost:2020/login',{
+    const response=await axios.post('https://chatvat-backend.onrender.com/login',{
       email:values.email,
       password:values.password,
     })
