5096d29a357f47d19610f9149281c2d6537b0703
Arshdeep Singh <arshdeep.intern@prepladder.com>
2025-05-27 23:49:12 +0530

    removelazyloading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React ,{ Suspense, lazy  } from 'react'
 
 import { Route, Routes } from 'react-router-dom'
-const Login=lazy(()=>import('./components/Login'))
-const Signup = lazy(() => import('./components/Signup'))
+import Login  from './components/Login.jsx';
+import Signup from './components/Signup'
 const Qrcode = lazy(() => import('./components/Qrcode'))
 const ChatApp = lazy(() => import('./components/Chat'))
 
 function App() {
 
-  
+
   return (
     <>
     <Suspense fallback={<div>loading.....</div>}>
