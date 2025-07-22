05f08b9af828ebdb51a4dbfefe433df965b640d6
PREPLADDER <prepladder@FVFH50Y0Q05P.local>
2025-07-22 23:59:58 +0530

    edf

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,7 +40,7 @@ const LogIn = () => {
 
   onSubmit :async(values) => {
           try{
-         const response=await axios.post('http://3.6.104.231:2020/login',{
+         const response=await axios.post('https://763a221bbcf4.ngrok-free.app/login',{
          email:values.email,
          password:values.password,
     })
